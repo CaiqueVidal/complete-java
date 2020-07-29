fea1b0a49774433778bf7ebbb1f60a0507686adb
CaiqueVidal <caique.freitas2@fatec.sp.gov.br>
2020-07-29 12:50:34 -0300

    findByDepartment implementation

--- src/21 - Acesso a banco de dados com JDBC/demo-dao-jdbc/application/Program.java
+++ src/21 - Acesso a banco de dados com JDBC/demo-dao-jdbc/application/Program.java
@@ -1,7 +1,10 @@
 package application;
 
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class Program {
@@ -13,6 +16,13 @@ public class Program {
 		System.out.println("--Test: seller findById--");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
+		
+		System.out.println("\n--Test: seller findByDepartment--");
+		Department department = new Department(2, null);
+		List<Seller> listSeller = sellerDao.findByDepartment(department);
+		for (Seller obj : listSeller) {
+			System.out.println(obj);
+		}
 
 	}
 
