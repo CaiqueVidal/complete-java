66da75db8592bf620302e2ad040ab0ba60764c92
CaiqueVidal <caique.freitas2@fatec.sp.gov.br>
2020-07-29 11:38:27 -0300

    SellerDaoJDBC and DaoFactory

--- src/21 - Acesso a banco de dados com JDBC/demo-dao-jdbc/application/Program.java
+++ src/21 - Acesso a banco de dados com JDBC/demo-dao-jdbc/application/Program.java
@@ -1,9 +1,13 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 
 	}
 
