820a1365021942d00e7c2c770c69054e1bdfa161
CaiqueVidal <caique.freitas2@fatec.sp.gov.br>
2020-07-29 12:31:08 -0300

    findById implementation

--- src/21 - Acesso a banco de dados com JDBC/demo-dao-jdbc/application/Program.java
+++ src/21 - Acesso a banco de dados com JDBC/demo-dao-jdbc/application/Program.java
@@ -2,12 +2,17 @@ package application;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
+		System.out.println("--Test: seller findById--");
+		Seller seller = sellerDao.findById(3);
+		System.out.println(seller);
 
 	}
 
--- src/21 - Acesso a banco de dados com JDBC/demo-dao-jdbc/model/dao/impl/SellerDaoJDBC.java
+++ src/21 - Acesso a banco de dados com JDBC/demo-dao-jdbc/model/dao/impl/SellerDaoJDBC.java
@@ -1,12 +1,25 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
 
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
 	@Override
 	public void insert(Seller obj) {
 		
@@ -27,8 +40,50 @@ public class SellerDaoJDBC implements SellerDao {
 
 	@Override
 	public Seller findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		try {
+			st = conn.prepareStatement(
+					"SELECT seller.*, department.Name as Depname "
+					+"FROM seller INNER JOIN department "
+					+"ON seller.DepartmentID = department.Id "
+					+"Where seller.id = ?");
+			
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			if (rs.next()) {
+				Department dep = instantiateDepartment(rs);	
+				Seller obj = instantiateSeller(rs, dep);
+				
+				return obj;
+			}
+			return null;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
 		
-		return null;
+	}
+
+	private Seller instantiateSeller(ResultSet rs, Department dep) throws SQLException {
+		Seller obj = new Seller();
+		obj.setId(rs.getInt("Id"));
+		obj.setName(rs.getString("Name"));
+		obj.setEmail(rs.getString("Email"));
+		obj.setBaseSalary(rs.getDouble("BaseSalary"));
+		obj.setBirthDate(rs.getDate("BirthDate"));
+		obj.setDepartament(dep);
+		return obj;
+	}
+
+	private Department instantiateDepartment(ResultSet rs) throws SQLException {
+		Department dep =new Department();
+		dep.setId(rs.getInt("DepartmentId"));
+		dep.setName(rs.getString("DepName"));
+		return dep;
 	}
 
 	@Override
