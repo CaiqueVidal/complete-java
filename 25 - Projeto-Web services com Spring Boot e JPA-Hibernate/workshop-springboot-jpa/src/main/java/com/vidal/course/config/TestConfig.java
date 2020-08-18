1d69b79f3e502db6568a3e1b5d7e054b8d692293
CaiqueVidal <caique.freitas2@fatec.sp.gov.br>
2020-08-18 14:03:52 -0300

    Many-to-many association with JoinTable

--- 25 - Projeto-Web services com Spring Boot e JPA-Hibernate/workshop-springboot-jpa/src/main/java/com/vidal/course/config/TestConfig.java
+++ 25 - Projeto-Web services com Spring Boot e JPA-Hibernate/workshop-springboot-jpa/src/main/java/com/vidal/course/config/TestConfig.java
@@ -50,6 +50,15 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategories().add(cat2);
+		p2.getCategories().add(cat1);
+		p2.getCategories().add(cat3);
+		p3.getCategories().add(cat3);
+		p4.getCategories().add(cat3);
+		p5.getCategories().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3,p4,p5));
+		
 		User u1 = new User(null, "Maria", "maria@gmail.com", "988887777", "1234");
 		User u2 = new User(null, "Alex", "alex@gmail.com", "977778888", "4321");
 		
