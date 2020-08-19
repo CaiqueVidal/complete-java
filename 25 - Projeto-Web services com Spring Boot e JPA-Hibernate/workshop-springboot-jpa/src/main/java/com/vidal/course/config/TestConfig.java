e998b0903d390e0c505e965d0a0a35337478f11f
CaiqueVidal <caique.freitas2@fatec.sp.gov.br>
2020-08-19 12:39:53 -0300

    OrderItem, many-to-many association with extra attributes

--- 25 - Projeto-Web services com Spring Boot e JPA-Hibernate/workshop-springboot-jpa/src/main/java/com/vidal/course/config/TestConfig.java
+++ 25 - Projeto-Web services com Spring Boot e JPA-Hibernate/workshop-springboot-jpa/src/main/java/com/vidal/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.vidal.course.entities.Category;
 import com.vidal.course.entities.Order;
+import com.vidal.course.entities.OrderItem;
 import com.vidal.course.entities.Product;
 import com.vidal.course.entities.User;
 import com.vidal.course.entities.enums.OrderStatus;
 import com.vidal.course.repositories.CategoryRepository;
+import com.vidal.course.repositories.OrderItemRepository;
 import com.vidal.course.repositories.OrderRepository;
 import com.vidal.course.repositories.ProductRepository;
 import com.vidal.course.repositories.UserRepository;
@@ -33,6 +35,9 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private ProductRepository productRepository;
+	
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -68,7 +73,13 @@ public class TestConfig implements CommandLineRunner {
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+		
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 	}
-	
 	
 }
