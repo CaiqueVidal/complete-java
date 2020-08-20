904abd0a9bc95cdeb37ac702ee2829f90537f9df
CaiqueVidal <caique.freitas2@fatec.sp.gov.br>
2020-08-20 11:28:55 -0300

    Payment, one-to-one association

--- 25 - Projeto-Web services com Spring Boot e JPA-Hibernate/workshop-springboot-jpa/src/main/java/com/vidal/course/config/TestConfig.java
+++ 25 - Projeto-Web services com Spring Boot e JPA-Hibernate/workshop-springboot-jpa/src/main/java/com/vidal/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.vidal.course.entities.Category;
 import com.vidal.course.entities.Order;
 import com.vidal.course.entities.OrderItem;
+import com.vidal.course.entities.Payment;
 import com.vidal.course.entities.Product;
 import com.vidal.course.entities.User;
 import com.vidal.course.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 }
